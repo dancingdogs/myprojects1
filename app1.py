da9999041ba5662ede4d7fbf4af7a9ba4bd10bfd
dancingdogs <70642626+dancingdogs@users.noreply.github.com>
2020-09-04 17:07:10 +0300

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,24 +1,41 @@
 import pandas as pd
+from openpyxl import load_workbook
+from xlrd import sheet
 
 data = pd.read_excel(r'C:\Users\User\Desktop\pyth1\week.xlsx')
 
 df = pd.DataFrame(data, columns=['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri', 'Sambata/Duminica'])
 
-df.to_excel(r'C:\Users\User\Desktop\pyth1\week2.xlsx', index = False, header=True)
+wb = load_workbook(filename='week.xlsx')
+sheet_ranges = wb['Sheet2']
+
+cell = (sheet_ranges.merged_cells.ranges)
+
+cell = sheet.cell(row=1, column=4)
+if type(cell).__name__ == 'MergedCell':
+  print("Oh no, the cell is merged!")
+else:
+  print("This cell is not merged.")
 
 
 
 
-#workbook = xlsxwriter.Workbook('week2.xlsx')
 
-#worksheet = workbook.add_worksheet()
 
-#workbook.close()
 
-'''
-worksheet.write(0, 0, 'Hello Excel')
+df2=df
 
-workbook.close()
 
-'''
 
+df2 = df2.style.set_properties(**{'font-weight': 'bold'})
+
+
+
+# workbook = xlsxwriter.Workbook('week3.xlsx')
+
+
+# bold = workbook.add_format({'bold': True})
+
+df2.to_excel(r'C:\Users\User\Desktop\pyth1\week2.xlsx', index=False, header=True)
+
+# workbook.close()
