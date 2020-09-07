afe677933d439b7ab03c557ae989fa4297c69b42
dancingdogs <70642626+dancingdogs@users.noreply.github.com>
2020-09-07 15:09:50 +0300

    filling unmerged cells in xlsx 2
    taking an excel file with merged cells with deleted information and parse it to another excel file filling the missing information

--- app1.py
+++ app1.py
@@ -1,6 +1,10 @@
 import pandas as pd
 from openpyxl import load_workbook
+from openpyxl.cell import Cell
 from xlrd import sheet
+from openpyxl import Workbook
+from openpyxl import worksheet as ws
+
 
 data = pd.read_excel(r'C:\Users\User\Desktop\pyth1\week.xlsx')
 
@@ -9,33 +13,36 @@ df = pd.DataFrame(data, columns=['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri', '
 wb = load_workbook(filename='week.xlsx')
 sheet_ranges = wb['Sheet2']
 
-cell = (sheet_ranges.merged_cells.ranges)
-
-cell = sheet.cell(row=1, column=4)
-if type(cell).__name__ == 'MergedCell':
-  print("Oh no, the cell is merged!")
-else:
-  print("This cell is not merged.")
-
-
-
-
-
-
-
+print(sheet_ranges.merged_cells.ranges)
 df2=df
 
+def testMerge(row, column):
+  cell=sheet_ranges.cell(row, column)
+  for mergedCell in sheet_ranges.merged_cells:
+    if cell.coordinate in mergedCell:
+      return True,mergedCell.start_cell.value
+  return False,0
 
+for index, row in df.iterrows():
+  if index==0:
+    continue
+  for i in range(len(df.columns)):
+    result,val=testMerge(index+1, i+1)
+    print(result,val)
+    if result:
+      df2.iloc[[index-1],[i]]= val
+
+
+''' 
+for index, row in df.iterrows():
+  print(row['Vineri'])
+'''
+
+#n=testMerge(2,5)
+#n2=testMerge(5,5)
 
 df2 = df2.style.set_properties(**{'font-weight': 'bold'})
 
-
-
-# workbook = xlsxwriter.Workbook('week3.xlsx')
-
-
-# bold = workbook.add_format({'bold': True})
-
 df2.to_excel(r'C:\Users\User\Desktop\pyth1\week2.xlsx', index=False, header=True)
 
-# workbook.close()
+
